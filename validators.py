51a200cd9dd55ef52931e8275c3ebcf0afdeeb5a
Diego Cervantez <diegocerva14@gmail.com>
2025-11-28 17:37:51 -0600

    Add date and categories validators

--- validators.py
+++ validators.py
@@ -1,4 +1,10 @@
 import re
-
 def is_valid_date(txt):
-    pattern = ""
+    pattern = r"^\d{2}/\d{2}/\d{4}$"
+    return re.search(pattern, txt) is not None
+
+ALLOWED_CATEGORIES = {"study", "fitness", "reading", "work", "other"}
+def is_valid_category(cat):
+    if cat is None:
+        return False
+    return cat.strip().lower() in ALLOWED_CATEGORIES
