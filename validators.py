3a91c4bb8d1486c4c7c75383443130f42edd3621
Diego Cervantez <diegocerva14@gmail.com>
2025-11-28 17:51:14 -0600

    Fix date format

--- validators.py
+++ validators.py
@@ -1,6 +1,6 @@
 import re
 def is_valid_date(txt):
-    pattern = r"^\d{2}/\d{2}/\d{4}$"
+    pattern = r"^\d{4}-\d{2}-\d{2}$"
     return re.search(pattern, txt) is not None
 
 ALLOWED_CATEGORIES = {"study", "fitness", "reading", "work", "other"}
