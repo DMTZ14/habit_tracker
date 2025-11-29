e5909745a730d696b00a0ea41539f99e43f8074a
Diego Cervantez <diegocerva14@gmail.com>
2025-11-28 18:48:11 -0600

    Add validators to Sessions Class

--- models.py
+++ models.py
@@ -1,17 +1,21 @@
+from validators import is_valid_category, is_valid_date
+
 class Sessions:
     def __init__(self, date, habit, category, minutes, note):
+        if not is_valid_date(date):
+            raise ValueError("Invalid date.")
+        if not habit.strip():
+            raise ValueError("Empty habit.")
+        if not is_valid_category(category):
+            raise ValueError("Invalid category.")
+        if minutes <= 0:
+            raise ValueError("Minutes must be > 0.")
+        if len(note) > 100:
+            raise ValueError("Note too long.")
         self.date = date
         self.habit = habit
         self.category= category
         self.minutes = minutes
         self.note = note
-
-date: str
-
-habit: str
-
-category: str
-
-minutes: int
-
-note: str | None
+    def __str__(self):
+        return f"{self.date}, {self.habit}, {self.category}, {self.minutes}, {self.note} "
